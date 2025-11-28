3950375e4f5f7defe3cd7cd7bb2f84ccaa281624
geilahtabanao <geilah_tabanao@dlsu.edu.ph>
2025-11-29 00:44:08 +0800

    Changed the title

--- pokemon_tcg_dashboard/app.py
+++ pokemon_tcg_dashboard/app.py
@@ -21,7 +21,7 @@ app = Dash(
         dbc.themes.BOOTSTRAP,
         "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
     ],
-    title="Pokémon TCG Dashboard"
+    title="Pokémon TCG Analytics: Gotta Price Em All"
 )
 
 # Timestamp changes every time app.py restarts
